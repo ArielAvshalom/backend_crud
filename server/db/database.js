cb97b551822d56b24fa8f6e2a9069487256a2419
ArielAvshalom <42783606+ArielAvshalom@users.noreply.github.com>
2022-01-20 16:45:45 -0500

    Update database.js

--- server/db/database.js
+++ server/db/database.js
@@ -2,10 +2,12 @@ const Sequelize = require('sequelize')
 const pkg = require('../../package.json')
 
 const db = new Sequelize(
-    process.env.DATABASE_url || `postgres://localhost:5432/{$pkg.name}`,
+    process.env.DATABASE_url || `postgres://postgres:sql@localhost:5432/${pkg.name}`,
     {
+        // user: 'postgres',
+        // password: 'sql',
         logging : false
     }
 )
 
-module.exports = dbpost
+module.exports = db
