6858c4d97858a085d9e1035f06bedc3c5d638aa4
ArielAvshalom <42783606+ArielAvshalom@users.noreply.github.com>
2022-01-20 15:29:53 -0500

    Create database.js

---
+++ server/db/database.js
@@ -0,0 +1,11 @@
+const Sequelize = require('sequelize')
+const pkg = require('../../package.json')
+
+const db = new Sequelize(
+    process.env.DATABASE_url || `postgres://localhost:5432/{$pkg.name}`,
+    {
+        logging : false
+    }
+)
+
+module.exports = db
