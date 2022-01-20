e40878cdba8d7e263245c0d1f94b0f41d01059c1
ArielAvshalom <42783606+ArielAvshalom@users.noreply.github.com>
2022-01-20 15:19:23 -0500

    Update index.js
    Will add api in next feature

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
 
-app.use('/api', require('./api'))
+//app.use('/api', require('./api'))
 
 app.listen(PORT, () =>
     console.log(`Serving portmanteau since there were ports ${PORT}`)
